6af2992f19879bb4b5da2bef42410c3745917321
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-07 23:26:06 +0500

    Add files via upload

---
+++ 01. DataTypes/examples.ts
@@ -0,0 +1,41 @@
+//Example-One (string)
+
+let author:string ="Mark Twain";
+let quote :string ="Good decisions come from experience. Experience comes from making bad decisions";
+console.log(`Author: ${author}\nQuote: ${quote}`);
+
+//Example-Two (number)
+
+let a:number =15;
+let b:number =3;
+console.log("a = 15 and b = 3");
+console.log(`a+b: ${a+b}`);
+console.log(`a-b: ${a-b}`);
+console.log(`a/b: ${a/b}`);
+console.log(`a*b: ${a*b}`);
+
+//Example-Three (boolean)
+
+let isRaining:boolean =true;
+let isSunny:boolean =false;
+console.log(`isRaining: ${isRaining}\nisSunny: ${isSunny}`);
+
+//Example-Four (undefined)
+
+let x;
+console.log(x); //Output: undefined
+
+//Example-Five (bigint)
+ 
+let bigIntValue:bigint= BigInt(4568907532367899999);
+console.log(bigIntValue);
+
+//Example-Six (null)
+
+let nullNumber: null=null;
+console.log(nullNumber);
+
+//Example-Seven (symbol)
+
+let mySymbol:symbol=Symbol("Madiha Ali");
+console.log(mySymbol);

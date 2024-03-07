b701c5de31cb823df6bbb16e3279cc586769e04b
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-07 23:18:50 +0500

    Add files via upload

---
+++ Array/Array.js
@@ -0,0 +1,11 @@
+//array of numbers
+var numbers = [1, 2, 3, 4, 5];
+//array of strings
+var fruits = ['Apple', 'Mango', 'Orange'];
+//Accessing elements of the array
+console.log(numbers[0], numbers[4]);
+console.log(fruits[0]);
+//Modifying elements of the aaray
+numbers[2] = 7;
+fruits[0] = 'Strawberry';
+console.log(numbers, fruits);

71a2152a4dd74c9e044015508702942b1ff3b700
MadihaAli1 <159538608+MadihaAli1@users.noreply.github.com>
2024-03-07 23:27:11 +0500

    Add files via upload

---
+++ 02. Variable/Variable.js
@@ -0,0 +1,7 @@
+//Cases
+var myName = "Madiha Ali"; //e.g of camelCase
+var favorite_color = "Black"; //e.g of snack_case
+var FavoriteFlower = "Tulips"; //e.g of PascalCase
+console.log(myName);
+console.log(favorite_color);
+console.log(FavoriteFlower);
---
+++ 02. Variable/Variable.ts
@@ -0,0 +1,9 @@
+//Cases
+
+let myName: string ="Madiha Ali"; //e.g of camelCase
+let favorite_color: string ="Black"; //e.g of snack_case
+let FavoriteFlower: string ="Tulips"; //e.g of PascalCase
+
+console.log(myName);
+console.log(favorite_color);
+console.log(FavoriteFlower);
